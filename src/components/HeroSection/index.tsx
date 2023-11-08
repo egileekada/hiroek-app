bbb9d4a039720cc81d9e9c979d9c0951321b2546
egileekada <egileoniso.ekada@gmail.com>
2023-11-08 14:32:52 +0100

    update on hero section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -12,7 +12,7 @@ export default function HeroSection(props: any) {
             <span className=" text-[#8C43FE] ">Empowered</span>.
           </p>
           <p className=" mt-4 ">
-          Hiroek is an app that gives you the platform to create and engage with communities and other social causes. Empowering a new generation of philanthropists, supporters, and action-takers. (Uniting thinkers, carers, and doers.)
+            Let change the intro on the website to;   Hiroek is an app that gives you the platform to create and engage with communities and other social causes. Empowering a new generation of philanthropists, supporters, and action-takers. (Uniting thinkers, carers, and doers.)
           </p>
           <button
             onClick={() => props.open(true)}
